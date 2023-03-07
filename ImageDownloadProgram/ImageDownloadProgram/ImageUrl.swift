88434ab6caf3d2df5159e3a3435434fdc821c819
hililyy <joun406@gmail.com>
2023-03-07 17:43:35 +0900

    [FIX] 이미지 URL 변경

--- ImageDownloadProgram/ImageDownloadProgram/ImageUrl.swift
+++ ImageDownloadProgram/ImageDownloadProgram/ImageUrl.swift
@@ -9,15 +9,15 @@ import Foundation
 
 enum ImageURL {
     private static let imageIds: [String] = [
-        "europe-4k-1369012",
-        "europe-4k-1318341",
-        "europe-4k-1379801",
-        "cool-lion-167408",
-        "iron-man-323408"
+        "6d63b4a30258e7a7201afb3475d022fd41d1a2caccd0c566eab28b91e2e5d306",
+        "6d63b4a30258e7a7201afb3475d022fd113e2bd2b7407c8202a97d2241a96625",
+        "6d63b4a30258e7a7201afb3475d022fd4022de826f725e10df604bf1b9725cfd",
+        "33a4e97c018d49e364f99718f188c89dac8e738cb631e72fdb9a96b36413984e",
+        "8fb89536158119f901780df1ba184931960f4ab09fe6e38bae8c63030c9b37f9"
     ]
     
     static subscript(index: Int) -> URL {
         let id = imageIds[index]
-        return URL(string: "https://wallpaperaccess.com/download/"+id)!
+        return URL(string: "https://item.kakaocdn.net/do/"+id)!
     }
 }
